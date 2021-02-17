cae3adba2f56bea536feb736ddc6ddd135306452
sflittrell <sflittrell@gmail.com>
2021-02-16 19:33:13 -0500

    added AM and PM function

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 
 
 function displayTime() {
-    const clock = document.getElementById('clockDisplay');
-    const time = new Date();
-    const hour = time.getHours();
-    const minute = time.getMinutes();
-    const second = time.getSeconds();
-    const amOrPm = 'AM';
-    const combinedTime = `${hour}:${minute}:${second} ${amOrPm}`;
+    // define all the variables and give them a value
+    let clock = document.getElementById('clockDisplay');
+    let time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let second = time.getSeconds();
+    let amOrPm = 'AM';
+    let combinedTime = `${hour}:${minute}:${second} ${amOrPm}`;
     clock.textContent = combinedTime;
+
+    // Sets AM of PM
+    if (hour >= 12) {
+        amOrPm = 'PM';
+    }
+
+    // Sets to standard time
+    if (hour > 12) {
+        hour = hour - 12;
+    }
 }
 displayTime();
 setInterval(displayTime, 1000);
